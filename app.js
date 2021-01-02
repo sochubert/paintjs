821ce162f2595ed5d57a17c911d2683be38649e2
MinjaeLee <sochubert@gmail.com>
2021-01-02 19:34:56 +0900

    js file

---
+++ app.js
@@ -0,0 +1,32 @@
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  
+}
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseDown(event) {
+  painting = true;
+}
+
+function onMouseUp(event) {
+  stopPainting();
+}
+
+function onMouseLeave(event) {
+  painting = false;
+}
+
+if(canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
